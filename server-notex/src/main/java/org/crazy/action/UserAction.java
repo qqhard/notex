84b17d25d4d28994bcbb705b621da3e949fb38ad
bolin.fbl <bolin.fbl@alibaba-inc.com>
2016-08-30 17:02:03 +0800

    add user action

--- server-notex/src/main/java/org/crazy/action/UserAction.java
+++ server-notex/src/main/java/org/crazy/action/UserAction.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 import org.crazy.model.User;
 import org.crazy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;

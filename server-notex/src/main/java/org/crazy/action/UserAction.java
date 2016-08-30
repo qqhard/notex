841735eeaef31d9691e8285dbbf161801611239f
bolin.fbl <bolin.fbl@alibaba-inc.com>
2016-08-30 23:11:33 +0800

    add code

--- server-notex/src/main/java/org/crazy/action/UserAction.java
+++ server-notex/src/main/java/org/crazy/action/UserAction.java
@@ -1,15 +1,20 @@
+
 package org.crazy.action;
 
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import org.crazy.form.RegisterForm;
 import org.crazy.model.User;
-import org.crazy.model.UserForm;
 import org.crazy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,51 +33,50 @@ public class UserAction {
     @Autowired  
     private  HttpServletRequest request;  
     
+
+    
     @RequestMapping(value = "/user/addUser", method = RequestMethod.POST)
-    public Object addUser(@RequestBody  User user){
-        
+    public Object addUser(@Valid RegisterForm registerForm, BindingResult bindlingResult){
+        PasswordEncoder passwordEncoder=new BCryptPasswordEncoder(4);
         HashMap<String, Object> rstMap = new HashMap<String, Object>();
         
-        if(user == null && user.getUsername() !=  null && user.getPassword() != null) {
-            User rstUser = userRepository.findByUsername(user.getUsername());
-            
-            if(rstUser != null) {
-                User rst = userRepository.save(user);
-                if(rst != null) {
-                    rstMap.put("success", true);
-                }
+        if (bindlingResult.hasFieldErrors()) {
+            List<FieldError> errors = bindlingResult.getFieldErrors();
+            rstMap.put("success", false);
+            for (FieldError error : errors) {
+                rstMap.put(error.getField(), error.getCode());
             }
-            else {
-                rstMap.put("success", false);
-                rstMap.put("msg", "username exist");
-            }
+            return rstMap;
+        }
+        
+        User user = userRepository.findByUsername(registerForm.getUsername());
+        
+        if(user != null) {
+            rstMap.put("success", false);
+            rstMap.put("msg", "username exist");
         }
         else {
-            rstMap.put("success", false);
-            rstMap.put("msg", "wrong arguments");
+            user = registerForm.update(new User());
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+            rstMap.put("success", true);
         }
         
         return rstMap;
     }
     
-    @RequestMapping(value = "/user/login", method = RequestMethod.POST)
-    public Object login(@RequestBody  UserForm userForm){
-        HttpSession session = request.getSession(); 
-        User user = userRepository.findByUsernameAndPassword(userForm.getUsername(), userForm.getPassword());
-        if(user == null) {
-            return false;
-        } 
-        else {
-            session.setAttribute("currUser", user);
-            return true;
-        }
-    }
     
-    @RequestMapping(value = "/user/logout", method = RequestMethod.POST)
-    public Object logout(@RequestBody  UserForm userForm) {
-        
-        HttpSession session = request.getSession();
-        session.invalidate();
-        return null;
-    }
+    
+
+    
+//    @RequestMapping(value = "/user/logout", method = RequestMethod.POST)
+//    public Object logout(@RequestBody  UserForm userForm) {
+//        //CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+//        //String token =  csrf.getToken();
+//        HttpSession session = request.getSession();
+//        session.invalidate();
+//        HashMap<String, Object> rstMap = new HashMap<String, Object>();
+//        rstMap.put("success", true);
+//        return rstMap;
+//    }
 }

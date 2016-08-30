97c23c57563270e2131eef743ea13532cd21c5a6
bolin.fbl <bolin.fbl@alibaba-inc.com>
2016-08-30 12:53:35 +0800

    add user mongo dao

---
+++ server-notex/src/main/java/org/crazy/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.crazy.repository;
+
+import org.crazy.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * 
+ * 类UserRepository.java的实现描述：用户DAO 
+ * @author bolin.fbl 2016年8月30日 下午12:52:02
+ */
+public interface UserRepository extends MongoRepository<User, String>{
+    public User findByUsernameAndPassword(String username, String password);
+}
